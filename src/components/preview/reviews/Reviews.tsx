800d33fcd2b38ecc7cd3eb456edb3e76e5d3a236
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-06-12 22:05:47 -0300

    set review text width to 100% when not on a large screen

--- src/components/preview/reviews/Reviews.tsx
+++ src/components/preview/reviews/Reviews.tsx
@@ -2,7 +2,7 @@ export default function Reviews() {
   return (
     <div className="text-center pt-10">
       <h2 className="text-3xl font-bold mb-10">Avaliações</h2>
-      <div className="w-1/2 m-auto mb-10">
+      <div className="lg:w-1/2 m-auto mb-10">
         <p className="italic mb-2">
           "Avaliação do Salão de Beleza: 100/100 Eu tive uma experiência excepcional no Salão de Beleza! Desde o momento em que entrei até o
           momento em que saí, fiquei impressionado com o profissionalismo, a atenção aos detalhes e o ambiente acolhedor que o salão
@@ -10,7 +10,7 @@ export default function Reviews() {
         </p>
         <h6 className="font-bold">Mary Smith</h6>
       </div>
-      <div className="w-1/2 m-auto mb-10">
+      <div className="lg:w-1/2 m-auto mb-10">
         <p className="italic mb-2">
           A equipe do salão foi extremamente talentosa e qualificada. Eles ouviram atentamente minhas preferências e necessidades, e
           realmente superaram minhas expectativas. Os serviços prestados foram de alta qualidade e o resultado final foi simplesmente
